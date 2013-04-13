24f27baf33c7daacb255b1f26d63fc863944d126
Mariusz Borsa <wrotki@msn.com>
2013-04-13 14:00:41 -0700

    Attempt to add a skybox (not working yet)

--- WebContent/js/skybox/SkyBox.js
+++ WebContent/js/skybox/SkyBox.js
@@ -1,25 +1,37 @@
+// https://github.com/mrdoob/three.js/wiki
+// http://stemkoski.github.io/Three.js/
+
 function SkyBox(origin){
+	this.origin = origin;
+}
+
+SkyBox.prototype.loadTextureCube = function(callback){
     var OB = window.OtherBrane;
     var path = OB.bucketsPath;
 // The model gets loaded by Actor, given the modelUrl 
     SkyBox.prototype.basePath = path;
-	this.origin = origin;
-}
-
-SkyBox.prototype.createMeshes = function(){
     var urlPrefix = SkyBox.prototype.basePath + "/3d/";
     var urls = [ urlPrefix + "posx.jpg", urlPrefix + "negx.jpg",
         urlPrefix + "posy.jpg", urlPrefix + "negy.jpg",
         urlPrefix + "posz.jpg", urlPrefix + "negz.jpg" ];
-    var textureCube = THREE.ImageUtils.loadTextureCube( urls );
+        
+    // Need to wait for texture load
+    var uvMapping = new THREE.UVMapping();
+    
+    SkyBox.prototype.textureCube = THREE.ImageUtils.loadTextureCube( urls, uvMapping, callback );
+}
+
+SkyBox.prototype.createMeshes = function(){
 
     var shader = THREE.ShaderUtils.lib["cube"];
     var uniforms = THREE.UniformsUtils.clone( shader.uniforms );
-    uniforms['tCube'].texture= textureCube;   // textureCube has been init before
+    uniforms['tCube'].texture= SkyBox.prototype.textureCube;   // textureCube has been init before
     var material = new THREE.ShaderMaterial({
         fragmentShader    : shader.fragmentShader,
         vertexShader  : shader.vertexShader,
-        uniforms  : uniforms
+        uniforms  : uniforms,
+        depthWrite: false,
+        side: THREE.BackSide
     });
     
     // build the skybox Mesh 
