64da7fbcae87cef48e7889aaa9840a9b09695051
Mariusz Borsa <wrotki@msn.com>
2013-04-28 22:16:07 -0700

    Fixed a couple of issues

--- WebContent/js/scene/FlyControls.js
+++ WebContent/js/scene/FlyControls.js
@@ -1,4 +1,4 @@
-define(["scene/three2"], function(){
+define(["scene/ThreeDebug"], function(){
 	
 /**
  * @author James Baicoianu / http://www.baicoianu.com/
@@ -204,7 +204,7 @@ THREE.FlyControls = function ( object, domElement ) {
 		this.object.translateZ( this.moveVector.z * moveMult );
 
 		this.tmpQuaternion.set( this.rotationVector.x * rotMult, this.rotationVector.y * rotMult, this.rotationVector.z * rotMult, 1 ).normalize();
-		this.object.quaternion.multiplySelf( this.tmpQuaternion );
+		this.object.quaternion.multiply( this.tmpQuaternion );
 
 		this.object.matrix.setPosition( this.object.position );
 		this.object.matrix.setRotationFromQuaternion( this.object.quaternion );
